b889304093b421050c7a91eb6b43c365fb2c8c58
root <mtrhoades0727@gmail.com>
2022-03-03 20:11:09 -1000

    make server!

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let http = require('http')
 let server = http.createServer(function (req, res) {
     res.writeHeader(206)
-    res.write('<h1>Hello!</h1><img src="https://httpstatusdogs.com/img/206.jpg" alt="206">')
+    res.write('<h1>PPPP arty til I die!</h1><img src="https://httpstatusdogs.com/img/206.jpg" alt="206">')
     res.end()
 })
 
